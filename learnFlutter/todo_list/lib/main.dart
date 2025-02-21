9f91b9e93ea3626541d48872b27e85e06088e2be
Pramartha <pramarthamahottama@gmail.com>
2025-02-21 23:02:57 +0800

    add buttons

--- learnFlutter/todo_list/lib/main.dart
+++ learnFlutter/todo_list/lib/main.dart
@@ -4,9 +4,14 @@ main() {
   runApp(ToDoList());
 }
 
-class ToDoList extends StatelessWidget {
+class ToDoList extends StatefulWidget {
   const ToDoList({super.key});
 
+  @override
+  State<ToDoList> createState() => _ToDoListState();
+}
+
+class _ToDoListState extends State<ToDoList> {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -15,18 +20,23 @@ class ToDoList extends StatelessWidget {
         length: 3,
         child: Scaffold(
           appBar: AppBar(
-            leading: Icon(Icons.book_rounded, size: 10),
-            title: Text(
-              "Your To Do List!",
-              style: TextStyle(
-                decoration: TextDecoration.underline,
-                fontSize: 20,
-                fontWeight: FontWeight.bold,
-              ),
-            ),
+            title: Text("Your To Do List!", style: TextStyle(fontSize: 20)),
             actions: [
-              Icon(Icons.menu),
-              Padding(padding: EdgeInsets.only(right: 5)),
+              Builder(
+                builder:
+                    (innerContext) => IconButton(
+                      onPressed: () {
+                        showDialog(
+                          context: innerContext,
+                          builder: (context) {
+                            return AddTaskDialog();
+                          },
+                        );
+                      },
+                      icon: Icon(Icons.add),
+                    ),
+              ),
+              SizedBox(width: 15),
             ],
             bottom: TabBar(
               tabs: [
@@ -39,12 +49,56 @@ class ToDoList extends StatelessWidget {
           body: TabBarView(
             children: [
               Center(child: Text("All")),
-              Center(child: Text("All")),
-              Center(child: Text("All")),
+              Center(child: Text("Done")),
+              Center(child: Text("Pending")),
             ],
           ),
         ),
       ),
+    );
+  }
+}
+
+class AddTaskDialog extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    TextEditingController taskController = TextEditingController();
+    return AlertDialog(
+      title: Text("Input New Task"),
+      content: SizedBox(
+        width: 40,
+        child: TextField(
+          controller: taskController,
+          decoration: InputDecoration(
+            prefixIcon: Icon(Icons.task),
+            hintText: "Give Your Task Name",
+            labelText: "New Task",
+            floatingLabelBehavior: FloatingLabelBehavior.always,
+            focusedBorder: OutlineInputBorder(
+              borderSide: BorderSide(color: Colors.blue),
+            ),
+            disabledBorder: OutlineInputBorder(
+              borderSide: BorderSide(color: Colors.black),
+            ),
+          ),
+        ),
+      ),
+      actions: [
+        TextButton(
+          onPressed: () {
+            Navigator.pop(context);
+          },
+          child: Icon(Icons.close, color: Colors.red),
+        ),
+        ElevatedButton(
+          onPressed: () {
+            String newTask = taskController.text;
+            print("Tugas Baru: $newTask");
+            Navigator.pop(context);
+          },
+          child: Icon(Icons.check, color: Colors.blue),
+        ),
+      ],
     );
   }
 }
