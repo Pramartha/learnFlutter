a5118e9788e7d7b768d722895e4392509913de7d
Pramartha <pramarthamahottama@gmail.com>
2025-02-22 22:54:40 +0800

    static To-DO List

--- learnFlutter/todo_list/lib/main.dart
+++ learnFlutter/todo_list/lib/main.dart
@@ -1,7 +1,14 @@
 import 'package:flutter/material.dart';
 
-main() {
-  runApp(ToDoList());
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(debugShowCheckedModeBanner: false, home: ToDoList());
+  }
 }
 
 class ToDoList extends StatefulWidget {
@@ -12,93 +19,139 @@ class ToDoList extends StatefulWidget {
 }
 
 class _ToDoListState extends State<ToDoList> {
+  List<Map<String, dynamic>> task = [];
+
+  void addTask(String taskName) {
+    setState(() {
+      task.add({"name": taskName, "status": false});
+    });
+  }
+
+  void removeTask(int index) {
+    setState(() {
+      task.removeAt(index);
+    });
+  }
+
+  void toggleTaskStatus(int index) {
+    setState(() {
+      task[index]["status"] = !task[index]["status"];
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      home: DefaultTabController(
-        length: 3,
-        child: Scaffold(
-          appBar: AppBar(
-            title: Text("Your To Do List!", style: TextStyle(fontSize: 20)),
-            actions: [
-              Builder(
-                builder:
-                    (innerContext) => IconButton(
-                      onPressed: () {
-                        showDialog(
-                          context: innerContext,
-                          builder: (context) {
-                            return AddTaskDialog();
-                          },
-                        );
-                      },
-                      icon: Icon(Icons.add),
-                    ),
+    return DefaultTabController(
+      length: 3,
+      child: Scaffold(
+        appBar: AppBar(
+          title: Center(
+            child: Text(
+              "Your To Do List!",
+              style: TextStyle(
+                fontSize: 20,
+                fontWeight: FontWeight.w500,
+                letterSpacing: 0.8,
               ),
-              SizedBox(width: 15),
-            ],
-            bottom: TabBar(
-              tabs: [
-                Tab(icon: Icon(Icons.menu), text: "All Task"),
-                Tab(icon: Icon(Icons.check), text: "Done"),
-                Tab(icon: Icon(Icons.timer), text: "Pending"),
-              ],
             ),
           ),
-          body: TabBarView(
-            children: [
-              Center(child: Text("All")),
-              Center(child: Text("Done")),
-              Center(child: Text("Pending")),
+          bottom: TabBar(
+            labelColor: Colors.white,
+            unselectedLabelColor: Colors.black,
+            indicator: UnderlineTabIndicator(
+              borderSide: BorderSide(color: Colors.white, width: 3),
+            ),
+            tabs: [
+              Tab(icon: Icon(Icons.menu), text: "All Task"),
+              Tab(icon: Icon(Icons.check), text: "Done"),
+              Tab(icon: Icon(Icons.timer), text: "Pending"),
             ],
+            dividerColor: Colors.white,
           ),
+          backgroundColor: Colors.blue,
+        ),
+        body: TabBarView(
+          physics: BouncingScrollPhysics(),
+          children: [
+            buildTaskList(task),
+            buildTaskList(task.where((task) => task["status"]).toList()),
+            buildTaskList(task.where((task) => !task["status"]).toList()),
+          ],
+        ),
+        floatingActionButton: FloatingActionButton(
+          onPressed: _showAddTaskDialog,
+          backgroundColor: Colors.blue,
+          child: Icon(Icons.add, color: Colors.white),
         ),
       ),
     );
   }
-}
 
-class AddTaskDialog extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
+  Widget buildTaskList(List<Map<String, dynamic>> taskList) {
+    return ListView.builder(
+      itemCount: taskList.length,
+      itemBuilder: (contex, index) {
+        return ListTile(
+          title: Text(taskList[index]["name"]),
+          leading: Checkbox(
+            value: taskList[index]["status"],
+            onChanged: (value) {
+              toggleTaskStatus(task.indexOf(taskList[index]));
+            },
+          ),
+          trailing: IconButton(
+            onPressed: () => removeTask(task.indexOf(taskList[index])),
+            icon: Icon(Icons.delete, color: Colors.red),
+          ),
+        );
+      },
+    );
+  }
+
+  void _showAddTaskDialog() {
     TextEditingController taskController = TextEditingController();
-    return AlertDialog(
-      title: Text("Input New Task"),
-      content: SizedBox(
-        width: 40,
-        child: TextField(
-          controller: taskController,
-          decoration: InputDecoration(
-            prefixIcon: Icon(Icons.task),
-            hintText: "Give Your Task Name",
-            labelText: "New Task",
-            floatingLabelBehavior: FloatingLabelBehavior.always,
-            focusedBorder: OutlineInputBorder(
-              borderSide: BorderSide(color: Colors.blue),
-            ),
-            disabledBorder: OutlineInputBorder(
-              borderSide: BorderSide(color: Colors.black),
+
+    showDialog(
+      context: context,
+      builder: (context) {
+        return AlertDialog(
+          title: Text("Input New Task"),
+          content: SizedBox(
+            width: MediaQuery.of(context).size.width * 0.5,
+            child: TextField(
+              controller: taskController,
+              decoration: InputDecoration(
+                prefixIcon: Icon(Icons.add_task),
+                hintText: "Give Your Task Name",
+                labelText: "New Task",
+                floatingLabelBehavior: FloatingLabelBehavior.always,
+                focusedBorder: OutlineInputBorder(
+                  borderSide: BorderSide(color: Colors.blue),
+                ),
+                enabledBorder: OutlineInputBorder(
+                  borderSide: BorderSide(color: Colors.black),
+                ),
+              ),
             ),
           ),
-        ),
-      ),
-      actions: [
-        TextButton(
-          onPressed: () {
-            Navigator.pop(context);
-          },
-          child: Icon(Icons.close, color: Colors.red),
-        ),
-        ElevatedButton(
-          onPressed: () {
-            String newTask = taskController.text;
-            print("Tugas Baru: $newTask");
-            Navigator.pop(context);
-          },
-          child: Icon(Icons.check, color: Colors.blue),
-        ),
-      ],
+          actions: [
+            TextButton(
+              onPressed: () => Navigator.pop(context),
+              child: Icon(Icons.close, color: Colors.red),
+            ),
+            ElevatedButton(
+              onPressed: () {
+                if (taskController.text.isNotEmpty) {
+                  addTask(taskController.text);
+                }
+                Navigator.pop(context);
+              },
+              style: ElevatedButton.styleFrom(backgroundColor: Colors.blue),
+              child: Icon(Icons.check, color: Colors.white),
+            ),
+          ],
+        );
+      },
     );
   }
 }
